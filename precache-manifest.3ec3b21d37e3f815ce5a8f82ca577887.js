1966ec98b78016fe39f4e471a0f4f0580f8082d8
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-10-28 10:58:27 -0300

    Updates

---
+++ precache-manifest.3ec3b21d37e3f815ce5a8f82ca577887.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5f54ecd4e56af3a4a056e9121a1432",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "88180e6071df14490c7e",
+    "url": "/portifolio/static/css/main.510fd51c.chunk.css"
+  },
+  {
+    "revision": "51b4711b52da4917257f",
+    "url": "/portifolio/static/js/2.7ae282d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portifolio/static/js/2.7ae282d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88180e6071df14490c7e",
+    "url": "/portifolio/static/js/main.8867883b.chunk.js"
+  },
+  {
+    "revision": "9ad50937adffe7b02ae7",
+    "url": "/portifolio/static/js/runtime-main.4d860e73.js"
+  },
+  {
+    "revision": "97ade63274f9db5e90b68322ed1e1ee0",
+    "url": "/portifolio/static/media/brain.97ade632.png"
+  },
+  {
+    "revision": "c75a9669b63368058be5d38826525f54",
+    "url": "/portifolio/static/media/git.c75a9669.png"
+  },
+  {
+    "revision": "3b4b1888c28cdab535fabe59432edf3f",
+    "url": "/portifolio/static/media/nowMe.3b4b1888.jpg"
+  }
+]);
